ef8aa13a4c476bdc86e89e1320e87b49a72e76a3
Alan Marx <alanmarx@hotmail.com>
2020-03-26 15:59:31 +0000

    Changed the renderTweets function to use prepend rather than append, also created a loadTweets function that loads all the tweets in the database onto the site upon the first page load

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -20,37 +20,10 @@ $(function() {
     })
   })
 
-
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense, donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const renderTweets = (tweetDatabase) => {
     for (const tweet of tweetDatabase) {
       const $tweet = createTweetElement(tweet);
-      $('#tweet-container').append($tweet);
+      $('#tweet-container').prepend($tweet);
     }
   };
   
@@ -77,11 +50,12 @@ $(function() {
     return $tweet;
   };
   
-  renderTweets(data);
+  const loadTweets = () => {
+    $.get("/tweets/", function(data) {
+      renderTweets(data);
+    })
+  };
 
-
-
-
-
+  loadTweets();
 });
  
